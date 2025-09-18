9b98c9d7ba9cfc5adcceb4ccf6af796b49790b1e
Prasad <aherprasad2811@gmail.com>
2025-09-18 14:39:44 +0000

    cpp

---
+++ CPP/input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string a;
+    int b;
+    cout<<"Enter a word\n";
+    cin>>a;
+    cout<<"Enter a number\n";
+    cin>>b;
+    cout<<a<<"\n"<<b<<"\n";
+    return 0;
+}
---
+++ CPP/name.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Prasad Vasant Aher";
+    return 0;
+}
