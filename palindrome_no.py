1776b578cdc59516eb49307ca6561b15f0fa1820
Prasad <aherprasad2811@gmail.com>
2025-09-18 08:22:50 +0000

    finals

--- palindrome_no.py
+++ palindrome_no.py
@@ -2,12 +2,12 @@ n = int(input("Enter a Palindrome"))
 m = n
 res = 0
 while m>0:
-        i = m%10
+        i = int(m%10)
         res = res*10+i
-        m = m/10
-        print(res)
+        m = int(m/10)
+        
 
 if res==n:
     print("is a palindrome")
 else:
-    print("not a palindrome"
+    print("not a palindrome")
