63efb9fade32722cd48e6af69b410b0800e36a85
Prasad <aherprasad2811@gmail.com>
2025-09-18 16:12:05 +0000

    pracs

--- CPP/input.cpp
+++ CPP/input.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main(){
     string a;
     int b;
+    float c;
     cout<<"Enter a word\n";
     cin>>a;
     cout<<"Enter a number\n";
     cin>>b;
-    cout<<a<<"\n"<<b<<"\n";
+    cout<<"Enter a decimal number\n";
+    cin>>c;
+    cout<<a<<"\n"<<b<<"\n"<<c;
     return 0;
 }
---
+++ CPP/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+    cout<<"Enter two numbers ";
+    cin>>a>>b;
+
+    cout<<"Sum is "<<a+b<<"\n";
+    return 0;
+}
