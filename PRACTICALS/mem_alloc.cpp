ed635027f2a7b67d3b9914f7d49d3230684eb2f0
Prasad <aherprasad2811@gmail.com>
2025-09-25 05:28:52 +0000

    memory allocation size on linux

---
+++ PRACTICALS/mem_alloc.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<unistd.h>
+using namespace std;
+
+int main(){
+    int a = 42;
+    cout<<"My Process ID is: "<<getpid()<<endl;
+    cout<<"Variable adderess: "<<&a<<endl;
+    cout<<"Enter to exit"<<endl;
+    cin.get();
+    return 0;
+}
